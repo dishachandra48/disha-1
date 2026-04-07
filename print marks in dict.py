885fd64eeaadd3849ca3b1e8ee860bb765e70af8
dishachandra48 <dishachandra48@gmail.com>
2026-04-07 21:28:39 +0530

    To print marks seperately in dict

---
+++ print marks in dict.py
@@ -0,0 +1,8 @@
+marks={}
+x=int(input("Enter marks of physics: "))
+marks.update({"physics":x})
+y=int(input("Enter marks of chemistry: "))
+marks.update({"chemistry":y})
+z=int(input("Enter marks of bio: "))
+marks.update({"bio":z})
+print(marks)
