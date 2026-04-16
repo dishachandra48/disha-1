7192b2c9bdc9575685c9dcf45d482b7eb3f61e80
dishachandra48 <dishachandra48@gmail.com>
2026-04-16 23:13:41 +0530

    Bank example

---
+++ bank.py
@@ -0,0 +1,21 @@
+class Account:
+    def __init__(self, bal, acc):
+        self.balance = bal
+        self.account_number = acc
+    
+    def debit(self, amount):
+        self.balance -= amount
+        print("Rs ", amount, "was debited")
+        print("Total balance is: ",self.get_balance())
+        
+    def credit(self, amount):
+        self.balance += amount
+        print("Rs ", amount, "was credited")
+        print("Total balance is: ",self.get_balance())    
+        
+    def get_balance(self):
+        return self.balance
+        
+acc1 = Account(10000,12345)        
+acc1.debit(20000)
+acc1.credit(105000)
